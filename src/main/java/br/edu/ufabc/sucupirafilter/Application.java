7a6f2f70e9d5fddd08cbc6e685b0aaae12428706
Isaac Pinheiro <isaacpnhr@gmail.com>
2018-04-30 16:48:10 -0300

    Primeiro commit.

---
+++ src/main/java/br/edu/ufabc/sucupirafilter/Application.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufabc.sucupirafilter;
+
+/**
+ *
+ * @author isaac
+ */
+public class Application {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Sucupira Filter");
+        
+    }
+    
+}
