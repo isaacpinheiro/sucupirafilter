2d5c0d0e3d5b7d3c13f2b101d00900d59eb30809
Isaac Pinheiro <isaacpnhr@gmail.com>
2018-04-30 22:15:14 -0300

    Iniciando a implementação da camada repository.

--- src/main/java/br/edu/ufabc/sucupirafilter/Application.java
+++ src/main/java/br/edu/ufabc/sucupirafilter/Application.java
@@ -9,11 +9,33 @@ package br.edu.ufabc.sucupirafilter;
  *
  * @author isaac
  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Application {
     
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    
     public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+    
+    @Bean
+    public CommandLineRunner runner() {
         
-        System.out.println("Sucupira Filter");
+        return (args) -> {
+            
+            log.info("Sucupira Filter");
+            // TODO
+            
+        };
         
     }
     
--- src/main/java/br/edu/ufabc/sucupirafilter/model/AreaAvaliacao.java
+++ src/main/java/br/edu/ufabc/sucupirafilter/model/AreaAvaliacao.java
@@ -12,12 +12,28 @@ package br.edu.ufabc.sucupirafilter.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "area_avaliacao")
 public class AreaAvaliacao implements Serializable {
     
     public static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+    
+    @Column(name = "nome")
     private String nome;
+    
+    @Column(name = "codigo")
     private String codigo;
     
     public AreaAvaliacao() {
