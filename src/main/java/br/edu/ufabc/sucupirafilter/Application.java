e4d4d40e86da6d75f0420e8fc038568968f14735
Isaac Pinheiro <isaacpnhr@gmail.com>
2018-05-07 14:02:06 -0300

    Implementando os métodos getProps e getPage.

--- src/main/java/br/edu/ufabc/sucupirafilter/Application.java
+++ src/main/java/br/edu/ufabc/sucupirafilter/Application.java
@@ -10,6 +10,15 @@ package br.edu.ufabc.sucupirafilter;
  * @author isaac
  */
 
+
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +27,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import br.edu.ufabc.sucupirafilter.model.AreaAvaliacao;
+import br.edu.ufabc.sucupirafilter.model.AreaConhecimento;
+import br.edu.ufabc.sucupirafilter.model.Curso;
+import br.edu.ufabc.sucupirafilter.model.Instituicao;
+import br.edu.ufabc.sucupirafilter.model.Programa;
+
+import br.edu.ufabc.sucupirafilter.repository.AreaAvaliacaoRepository;
+import br.edu.ufabc.sucupirafilter.repository.AreaConhecimentoRepository;
+import br.edu.ufabc.sucupirafilter.repository.CursoRepository;
+import br.edu.ufabc.sucupirafilter.repository.InstituicaoRepository;
+import br.edu.ufabc.sucupirafilter.repository.ProgramaRepository;
+
 @SpringBootApplication
 public class Application {
     
@@ -25,6 +46,28 @@ public class Application {
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+    
+    public static Properties getProp(String fileName) throws IOException {
+        Properties props = new Properties();
+        FileInputStream file = new FileInputStream(fileName);
+        props.load(file);
+        return props;
+    }
+    
+    public static String getPage(URL url) throws IOException {
+        
+        String page = "";
+        String inputLine;
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        
+        while ((inputLine = in.readLine()) != null) {
+            page += (inputLine + "\n");
+        }
+        
+        in.close();
+        return page;
+        
     }
     
     @Bean
